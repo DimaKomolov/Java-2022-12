d0f2b6f40baefe00f5f679f018365e2dd00dc498
Dmitriy <komolov.dima@bk.ru>
2023-07-26 23:35:18 +0300

    hw-06

---
+++ hw06-atm/src/main/java/api/AtmCassette.java
@@ -0,0 +1,17 @@
+package api;
+
+
+
+import model.Denomination;
+
+import java.util.List;
+
+public interface AtmCassette {
+    Denomination denomination();
+
+    void put(List<Bill> bills);
+
+    Bill take();
+
+    int count();
+}
